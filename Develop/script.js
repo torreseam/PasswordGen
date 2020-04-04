524f2237e383744c8058b272cbe246c4bdf43f50
torreseam <615694479+torreseam@users_noreply.github.com>
2020-04-03 23:30:07 -0500

    update style

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,33 @@
-// Assignment code here
+// concatenate password values
+
+var passwordLenght = 0;
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOQRSTUVWXYZ";
+var specialCharacter = "~!@#$%^&*()_+{}|:<>?,.';'[]\=-`";
+var numberValue = "123456789";
+
 
 
 var generatePassword = function(){
-  
+  passGroup = "";
+  upperPassword = "";
+  passwordLenght = promptPasswordLenght();
 }
+
+
+var get = document.querySelector("#generate");
+
+get.addEventListener("click", function () {
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+});
+
+var generatePassword = window.prompt("PromptPasswordLenght");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(passwordLenght);
+
 
 
 
@@ -22,3 +46,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+for (var i = 0; i < enter; i++) {
+  var pickChoices = choice[Math.floor(Math.random() * choice.length())];
+  password.push(pickChoices);
+}
+
+var ps = password.join("");
+UserInput(ps);
+return ps;
+};
+
+function UserInput(ps) {
+  document.getElementById("password").textContent = ps;
+
+}
