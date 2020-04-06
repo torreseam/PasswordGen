5b7c42fce04e193303525d2517a722651dc40fcc
torreseam <615694479+torreseam@users_noreply.github.com>
2020-04-05 22:08:52 -0500

    update lines

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 //Character Array
 var lowerArr = 'abcdefghijklmnopqrstuvwxyz';
 var upperArr = 'ABCDEFGHIJKLMNOQRSTUVWXYZ';
-var symArr = '~!@#$%^&*()_+{}|:<>?,.';'[]\=-`';
+var symArr = '~!@#$%^&*()_+{}|:<>?,.'; '[]\=-`';
 var numArr = '1234567890';
 lowercaseInput = document.getElementById("lowercase");
 uppercaseInput = document.getElementById("uppercase");
@@ -25,76 +25,74 @@ passwordLengthInput = document.getElementById("passwordLength")
 //Generate Password Function
 function generatePassword() {
   var passwordLength = Number(prompt("How many characters would you like in your passowrd (Choose between 8 to 128)"));
-  console.log(passwordLength);
+  console.log(passwordLength)
+};
+if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+  passwordLength = Number(prompt("Invalid Request, try again!"));
 
-  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    passwordLength = Number(prompt("Invalid Request, try again!"));
+  var lowercase = confirm("Do you want all LowerCase?");
+  var uppercase = confirm("Do you want all UpperCase?");
+  var numbers = confirm("Do you want numbers?");
+  var symbols = confirm("Do you want symbols?");
 
-    var lowercase = confirm("Do you want all LowerCase?");
-    var uppercase = confirm("Do you want all UpperCase?");
-    var numbers = confirm("Do you want numbers?");
-    var symbols = confirm("Do you want symbols?");
-
-    if (!(lowercase || uppercase || numbers || symbols)) {
-      alert("Pick one type");
-      lowercase = confirm("Do you want all LowerCase?");
-      uppercase = confirm("Do you want all UpperCase?");
-      numbers = confirm("Do you want numbers?");
-      symbols = confirm("Do you want symbols?");
-      console.log(lowercase, uppercase, numbers, symbols)
-    }
-    var passwordOptions = {
-      lenght: passwordLenght,
-      hasSpecialCharacters: symbols,
-      hasNumericCharacters: numbers,
-      hasLowercaseCharacters: lowercase,
-      hasUppercaseCharacters: uppercase,
-    }
-  };
-  return passwordOptions
-
-  function generate() {
-    var passwordOptions = generatePassword();
-    var passwordChar = [];
-    var lowercaseChar = [];
-    if (passwordOptions.hasLowercaseCharacters) {
-      for (var i = 0; i < passwordOptions.lenght; i++) {
-        var lowercaseChar = lowerArr[Math.floor(Math.random() * passwordOptions.lenght)];
-        passwordChar.push(lowercaseChar[i]);
-        console.log("lowercaseChar"[i]);
-      }
-    };
-    var symbolChar = [];
-    if (passwordOptions.hasSpecialCharacters) {
-      for (var i = 0; i < passwordOptions.lenght; i++) {
-        var symbolChar = symArr[Math.floor(Math.random() * passwordOptions.lenght)];
-        passwordChar.push("symbolsChar"[i]);
-        console.log(symbolChar);
-      }
-    };
-    var uppercaseChar = [];
-    if (passwordOptions.hasuppercaseCharacters) {
-      for (var i = 0; i < passwordOptions.lenght; i++) {
-        var uppercaseChar = upperArr[Math.floor(Math.random() * passwordOptions.lenght)];
-        passwordChar.push("uppercaseChar"[i]);
-        console.log(uppercaseChar);
-      }
-    };
-    var numbersChar = [];
-    if (passwordOptions.hasNumericCharacters) {
-      for (var i = 0; i < passwordOptions.lenght; i++) {
-        var numbersChar = numArr[Math.floor(Math.random() * passwordOptions.lenght)];
-        passwordChar.push("numberChar"[i]);
-        console.log(numberChar);
-      }
-    };
-    var generateValue = [];
-    for (var i = 0; i < passwordOptions.lenght; i++) {
-      var passwordResult = passwordChar[Math.floor(Math.random() * passwordOptions.length)];
-      generateValue.push(passwordResult);
-      console.log(passwordResult = generateValue);
-    }
+  if (!(lowercase || uppercase || numbers || symbols)) {
+    alert("Pick one type");
+    lowercase = confirm("Do you want all LowerCase?");
+    uppercase = confirm("Do you want all UpperCase?");
+    numbers = confirm("Do you want numbers?");
+    symbols = confirm("Do you want symbols?");
+    console.log(lowercase, uppercase, numbers, symbols)
   }
+  var passwordOptions = {
+    lenght: passwordLenght,
+    hasSpecialCharacters: symbols,
+    hasNumericCharacters: numbers,
+    hasLowercaseCharacters: lowercase,
+    hasUppercaseCharacters: uppercase,
+  }
+  return passwordOptions.join('');
+    function generate() {
+      var passwordOptions = generatePassword();
+      var passwordChar = [];
+      var lowercaseChar = [];
+      if (passwordOptions.hasLowercaseCharacters) {
+        for (var i = 0; i < passwordOptions.lenght; i++) {
+          var lowercaseChar = lowerArr[Math.floor(Math.random() * passwordOptions.lenght)];
+          passwordChar.push(lowercaseChar[i]);
+          console.log("lowercaseChar"[i]);
+        }
+      };
+      var symbolChar = [];
+      if (passwordOptions.hasSpecialCharacters) {
+        for (var i = 0; i < passwordOptions.lenght; i++) {
+          var symbolChar = symArr[Math.floor(Math.random() * passwordOptions.lenght)];
+          passwordChar.push("symbolsChar"[i]);
+          console.log(symbolChar);
+        }
+      };
+      var uppercaseChar = [];
+      if (passwordOptions.hasuppercaseCharacters) {
+        for (var i = 0; i < passwordOptions.lenght; i++) {
+          var uppercaseChar = upperArr[Math.floor(Math.random() * passwordOptions.lenght)];
+          passwordChar.push("uppercaseChar"[i]);
+          console.log(uppercaseChar);
+        }
+      };
+      var numbersChar = [];
+      if (passwordOptions.hasNumericCharacters) {
+        for (var i = 0; i < passwordOptions.lenght; i++) {
+          var numbersChar = numArr[Math.floor(Math.random() * passwordOptions.lenght)];
+          passwordChar.push("numberChar"[i]);
+          console.log(numberChar);
+        }
+      };
+      var generateValue = [];
+      for (var i = 0; i < passwordOptions.lenght; i++) {
+        var passwordResult = passwordChar[Math.floor(Math.random() * passwordOptions.length)];
+        generateValue.push(passwordResult);
+        console.log(passwordResult = generateValue);
+      }
+    }
   get.addEventListener("click", writePassword);
 
   function passResult() {
@@ -102,3 +100,4 @@ function generatePassword() {
     var paswordQuery = document.querySelector("#password");
     passwordQuery.value = password;
   }
+};
