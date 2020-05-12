815c361bb052edceeede4ed5e127b6be22642f9b
torreseam <615694479+torreseam@users_noreply.github.com>
2020-05-11 20:15:57 -0500

    Update js functionality

--- Develop/script.js
+++ Develop/script.js
@@ -17,76 +17,103 @@ function generatePassword() {
   var passwordLength = Number(prompt("How many characters would you like in your passowrd (Choose between 8 to 128)"));
   console.log(passwordLength)
 
-  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128)
-    passwordLength = Number(prompt("Invalid, try again!"));
+  // if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
+  //   passwordLength = Number(prompt("Invalid, try again!"));
+  // }
 
   var lowercase = confirm("Do you want all LowerCase?");
   var uppercase = confirm("Do you want all UpperCase?");
   var numbers = confirm("Do you want numbers?");
   var symbols = confirm("Do you want symbols?");
 
-  if (!(lowercase && uppercase && numbers && symbols)) {
-    alert("Pick one type");
-    lowercase = confirm("Do you want all LowerCase?");
-    uppercase = confirm("Do you want all UpperCase?");
-    numbers = confirm("Do you want numbers?");
-    symbols = confirm("Do you want symbols?");
-    // console.log(lowercase, uppercase, numbers, symbols);
-  }
+  console.log("-->", lowercase, uppercase, numbers, symbols)
+  // if (!(lowercase && uppercase && numbers && symbols)) {
+  //   alert("Pick one type");
+  //   lowercase = confirm("Do you want all LowerCase?");
+  //   uppercase = confirm("Do you want all UpperCase?");
+  //   numbers = confirm("Do you want numbers?");
+  //   symbols = confirm("Do you want symbols?");
+  //   console.log(lowercase, uppercase, numbers, symbols);
+  // }
   var passcodeOptions = {
-    length: passwordLength,
+    size: passwordLength,
     hasSpecialCharacters: symbols,
     hasNumericCharacters: numbers,
     hasLowercaseCharacters: lowercase,
     hasUppercaseCharacters: uppercase,
   };
-  return passcodeOptions;
+  //return passcodeOptions;
+  generate(passcodeOptions)
 }
-function generate() {
-  var passwordOptions = generatePassword();
+
+function generate(passwordOptions) {
+ // var passwordOptions = generatePassword();
   var passwordChar = [];
   var lowercaseChar = [];
   var uppercaseChar = [];
   var numberChar = [];
   var symbolChar = [];
+  var choices = []
 
   if (passwordOptions.hasLowercaseCharacters) {
-    for (var i = 0; i < passcodeOptions.length; i++) {
-      var lowercaseChar = lowerArr[Math.floor(Math.random() * passcodeOptions.length)];
-      passwordChar.push(lowercaseChar);
-      console.log("lowercaseChar");
-    }
+    console.log("low")
+    passwordChar.push(lowerArr[Math.floor(Math.random() * lowerArr.length)])
+    choices = choices.concat(lowerArr)
+    console.log("1: ", passwordChar)
+    // for (var i = 0; i < passcodeOptions.length; i++) {
+    //   var lowercaseChar = lowerArr[Math.floor(Math.random() * passcodeOptions.length)];
+    //   passwordChar.push(lowercaseChar);
+    //   console.log("lowercaseChar");
+    // }
   }
 
-  if (passcodeOptions.hasuppercaseCharacters) {
-    for (var i = 0; i < passcodeOptions.length; i++) {
-      var uppercaseChar = upperArr[Math.floor(Math.random() * passcodeOptions.length)];
-      passwordChar.push(uppercaseChar);
-      console.log(uppercaseChar);
-    }
+  console.log("bvefore 2: ", passwordOptions.hasUppercaseCharacters)
+  if (passwordOptions.hasUppercaseCharacters) {
+
+    passwordChar.push(upperArr[Math.floor(Math.random() * upperArr.length)])
+    choices = choices.concat(upperArr)
+    console.log("2: ", passwordChar)
+    // for (var i = 0; i < passcodeOptions.length; i++) {
+    //   var uppercaseChar = upperArr[Math.floor(Math.random() * passcodeOptions.length)];
+    //   passwordChar.push(uppercaseChar);
+    //   console.log(uppercaseChar);
+    // }
   }
 
-  if (passcodeOptions.hasNumericCharacters) {
-    for (var i = 0; i < passcodeOptions.length; i++) {
-      var numberChar = numbArr[Math.floor(Math.random() * passcodeOptions.length)];
-      passwordChar.push(numberChar);
-      // console.log(numberChar);
-    }
+  if (passwordOptions.hasNumericCharacters) {
+    passwordChar.push(numbArr[Math.floor(Math.random() * numbArr.length)])
+    choices = choices.concat(numbArr)
+    console.log("3: ", passwordChar)
+    // for (var i = 0; i < passcodeOptions.length; i++) {
+    //   var numberChar = numbArr[Math.floor(Math.random() * passcodeOptions.length)];
+    //   passwordChar.push(numberChar);
+    //   console.log(numberChar);
+    // }
   }
 
   if (passwordOptions.hasSpecialCharacters) {
-    for (var i = 0; i < passcodeOptions.length; i++) {
-      var symbolChar = symArr[Math.floor(Math.random() * passcodeOptions.length)];
-      passwordChar.push(symbolChar);
-      // console.log(symbolChar);
-    }
+    passwordChar.push(symbArr[Math.floor(Math.random() * symbArr.length)])
+    choices = choices.concat(symbArr)
+    console.log("4: ", passwordChar)
+    // for (var i = 0; i < passcodeOptions.length; i++) {
+    //   var symbolChar = symArr[Math.floor(Math.random() * passcodeOptions.length)];
+    //   passwordChar.push(symbolChar);
+    //   // console.log(symbolChar);
+    // }
   }
 
-  for (i = 0; i < input; i++) {
-    choices.push(inputCharSet[Math.floor((Math.random() * inputCharSet.length) + 0)]);
-    var results = choices.join("");
+  var resto = passwordOptions.size - passwordChar.length
+  console.log(resto, choices)
+  for (i = 0; i < resto; i++) {
+    passwordChar.push(choices[Math.floor(Math.random() * choices.length)])
+
   }
-  return (results);
+  console.log("pass array: ", passwordChar)
+  passwordText.value = passwordChar.join("")
+  console.log(passwordText)
+
+
+  // return (results);
 };
 
 
